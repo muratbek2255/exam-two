1ee2606674b88b579a043ee1a9793600feca131e
muratbek2255 <muratbek2255@gmail.com>
2022-01-18 18:43:52 +0600

    h
    hello

--- exam_two/task4.py
+++
@@ -1,8 +0,0 @@
-def func(a, b, c):
-    return a+b+c
-
-print(func(a=7, b=8, c=9))
-
-
-
-
---
+++ task4.py
@@ -0,0 +1,11 @@
+import numbers
+
+def func(*args):
+    result = 0
+    for i in args:
+        if isinstance(i, numbers.Number):
+            result = result + i
+    return result
+
+
+
