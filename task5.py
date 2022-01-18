0bc021cb343b1da2e3f262e46563e484ed33e94b
muratbek2255 <muratbek2255@gmail.com>
2022-01-18 21:17:57 +0600

    H

--- task5.py
+++ task5.py
@@ -42,83 +42,83 @@
 # print(f'Продуктивные: {product}'
 #       f'Непродуктивные: {noproduct}')
 
-class Office:
-    def __init__(self, name, id, time):
-        self.name = name
-        self.id = id
-        self.time = time
-
-    def salary(self):
-        pass
-
-    def summa(self):
-        pass
-
-    def KPD(self):
-        self.time*100/40
-
-
-class Manager(Office):
-    def init(self, name, id, time, money):
-        super().__init__(name, id, time)
-        self.money = money
-
-    def salary(self):
-        return self.money
-
-class Secretary(Office):
-    def init(self, name, id, time, salary):
-        super().__init__(name, id, time)
-        self.salary = salary
-
-    def salary(self):
-        return self.salary
-
-class SalesMan(Office):
-    def init(self, name, id, time, salary, prodaja) -> None:
-        super().__init__(name, id, time)
-        self.salary = salary
-        self.prodaja = prodaja
-
-    def salary(self):
-        return self.salary + (self.prodaja*50)
-
-class ShopWorker(Office):
-    def init(self, name, id, time, salary, hour):
-        super().__init__(name, id, time)
-        self.salary = salary
-        self.hour = hour
-
-    def salary(self):
-        return self.hour * 100
-
-class SecretaryReplacement(Office):
-    def init(self, name, id, time, salary, hour):
-        super().__init__(name, id, time)
-        self.salary = salary
-        self.hour = hour
-
-    def salary(self):
-        return self.hour * 100
-
-
-manager = Manager("Барсбек Канаткулов", 1, 18, 45000)
-secretary = Secretary("Алымкул Тилекбаев",2,38, 20, 20000)
-sales_person = SalesMan("Айпери Шалымбекова", 3, 38)
-cex = ShopWorker("Бакыт Рустамов",4,25)
-cex2 = ShopWorker("Алтынай Ширинбаева",5,40)
-zamena = SecretaryReplacement("Жанар Рыскулов",6,33)
-personal = []
-def office_money():
-    total_money = sum([
-        person.salary() for person in personal
-        ])
-    return f"Общий заработок персонала - {total_money}"
-def vyvod():
-    total = office_money()
-    for worker in personal:
-        print(f'ID - {worker.id}, Name - {worker.name} Salary - {worker.salary} Perfomance - {worker.KPD()}')
-    return total
-vyvod()
+# class Office:
+#     def __init__(self, name, id, time):
+#         self.name = name
+#         self.id = id
+#         self.time = time
+#
+#     def salary(self):
+#         pass
+#
+#     def summa(self):
+#         pass
+#
+#     def KPD(self):
+#         self.time*100/40
+#
+#
+# class Manager(Office):
+#     def init(self, name, id, time, money):
+#         super().__init__(name, id, time)
+#         self.money = money
+#
+#     def salary(self):
+#         return self.money
+#
+# class Secretary(Office):
+#     def init(self, name, id, time, salary):
+#         super().__init__(name, id, time)
+#         self.salary = salary
+#
+#     def salary(self):
+#         return self.salary
+#
+# class SalesMan(Office):
+#     def init(self, name, id, time, salary, prodaja) -> None:
+#         super().__init__(name, id, time)
+#         self.salary = salary
+#         self.prodaja = prodaja
+#
+#     def salary(self):
+#         return self.salary + (self.prodaja*50)
+#
+# class ShopWorker(Office):
+#     def init(self, name, id, time, salary, hour):
+#         super().__init__(name, id, time)
+#         self.salary = salary
+#         self.hour = hour
+#
+#     def salary(self):
+#         return self.hour * 100
+#
+# class SecretaryReplacement(Office):
+#     def init(self, name, id, time, salary, hour):
+#         super().__init__(name, id, time)
+#         self.salary = salary
+#         self.hour = hour
+#
+#     def salary(self):
+#         return self.hour * 100
+#
+#
+# manager = Manager("Барсбек Канаткулов", 1, 18, 45000)
+# secretary = Secretary("Алымкул Тилекбаев",2,38, 20, 20000)
+# sales_person = SalesMan("Айпери Шалымбекова", 3, 38)
+# cex = ShopWorker("Бакыт Рустамов",4,25)
+# cex2 = ShopWorker("Алтынай Ширинбаева",5,40)
+# zamena = SecretaryReplacement("Жанар Рыскулов",6,33)
+# personal = []
+# def office_money():
+#     total_money = sum([
+#         person.salary() for person in personal
+#         ])
+#     return f"Общий заработок персонала - {total_money}"
+# def vyvod():
+#     total = office_money()
+#     for worker in personal:
+#         print(f'ID - {worker.id}, Name - {worker.name} Salary - {worker.salary} Perfomance - {worker.KPD()}')
+#     return total
+# vyvod()
 
 
